ac66531f38f3bbf61814da4e8546403cfd2c495b
rebootr <rkok@outlook.com>
2014-12-15 21:33:46 +0100

    Initial release

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kickass</title>
+</head>
+<body>
+	<form method="GET" action="search.php">
+		<h1>Kickass.so Search</h1>
+		<p>Enter search string:
+			<input type="text" name="search_string"><input type="submit" value="Go!">
+		</p>
+	</form>
+</body>
+</html>
